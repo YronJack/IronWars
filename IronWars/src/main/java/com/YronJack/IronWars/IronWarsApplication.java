9c3343c4ddd1877cc6f357db5d4f17a5844c7176
afminguela <afminguela@gmail.com>
2025-08-29 12:01:35 +0200

    git ignore revisado

---
+++ IronWars/src/main/java/com/YronJack/IronWars/IronWarsApplication.java
@@ -0,0 +1,13 @@
+package com.YronJack.IronWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronWarsApplication.class, args);
+	}
+
+}
---
+++ IronWars/src/test/java/com/YronJack/IronWars/IronWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YronJack.IronWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
