749a2a80cc7dcef8b9781ebcb3c06bfd7162059b
afminguela <afminguela@gmail.com>
2025-09-03 10:43:15 +0200

    Commit after Merging Exams,Teacher and languaje

--- IronWars/src/main/java/com/YronJack/IronWars/controller/ExamController.java
+++ IronWars/src/main/java/com/YronJack/IronWars/controller/ExamController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/exams")
+@RequestMapping("/api/exams")
 public class ExamController {
     @Autowired
     private ExamServiceImpl examService;
